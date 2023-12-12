9e0c6b15c89e5622da2b8f6a290a2745ede0ae85
TrungHa <Trung.TrongHa@coxautoinc.com>
2023-12-12 15:04:24 -0600

    First commit

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -4,15 +4,19 @@
 
 # Step 1:
 # This is your Docker ID/path
-# dockerpath=<>
+dockerpath="hatrongtrung/project4trunght:v1.0.0"
 
 # Step 2
 # Run the Docker Hub container with kubernetes
-
+kubectl run project4trunght\
+    --image=$dockerpath\
+    --port=80 --labels app=project4trunght
 
 # Step 3:
 # List kubernetes pods
+kubectl get pods
 
 # Step 4:
 # Forward the container port to a host
+kubectl port-forward project4trunght 8000:80
 
